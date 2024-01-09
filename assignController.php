476517dd7509a7b9ccb6bddffc1dca4b5b0939d4
albannakhaled <albannakhalil90@gmail,com>
2024-01-09 19:44:51 +0200

    fix table in php

--- assignController.php
+++ assignController.php
@@ -1,23 +1,31 @@
 <?php
-// When a new attendance is created
-include_once('initSession.php');
+//when a new attendance is created
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['projectSelected']) && isset($_POST['select-student'])) {
-        $selectedProject = $_POST['projectSelected'];
+include_once ('initSession.php');  
 
-        // Initialize the project record if not exists
-        if (!isset($projectsManager->record[$selectedProject])) {
-            $projectsManager->record[$selectedProject] = array();
-        }
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+	if (isset($_POST['projectSelected']) && isset($_POST['select-student']))
+	{
+		
+		foreach($_POST['select-student'] as $check)
+		{
 
-        foreach ($_POST['select-student'] as $studentKey) {
-            // Add the selected student to the project record
-            $projectsManager->record[$selectedProject][] = $projectsManager->listOfStudents[$studentKey];
-        }
-    } else {
-        echo "You should enter a sheet date";
-    }
+			foreach ($projectsManager->listOfStudents as $key => $value)
+			{
+				
+				if($key == $check)
+				{	
+				
+					$projectsManager->record[$_POST['projectSelected']][] = array($value->firstName, $value->lastName);
+				}
+				
+			}	
+		
+		}	
+	}
+	else 
+		echo "You should to enter a sheet date";
 }
-
 include_once("index.php");
+?>
--- displayProjectWithStudent.php
+++ displayProjectWithStudent.php
@@ -1,34 +1,54 @@
 <?php
-include_once ('initSession.php');
+include_once('initSession.php');
 
-//to dispaly the sheets table
-echo "<table border=1>";
-echo "<tr><td></td>";
+// Check if the session variable is set
+if (isset($_SESSION['projectManager'])) {
+    // Access the project manager object from the session
+    $projectsManager = $_SESSION['projectManager'];
+    
+    // Print the records in the project manager
+    print_r($projectsManager->record);
 
-foreach($projectsManager->listOfStudents as $key => $value ) {  // display the students names in the first row
-	echo "<td>".$value->firstName."</td>";
-}
-
-foreach( $projectsManager->listOfProjects as $key => $value) { 		// display the projects names in the first column
-	 
-	echo "<tr>";
-	echo "<td>". $value->name."</td>";
-	$concat = array($value->firstName,$value->lastName) ;
-	
-		foreach($projectsManager->record as $key => $value ){        
-        // display the X if the student is assigned to the project
-            if(in_array($concat,$value)){
-                echo "<td>X</td>";
-            }else{
-                echo "<td></td>";
+    // Display the project selected with students in a table
+    ?>
+    <table border="1">
+        <tr>
+            <th></th>
+            <?php
+            // Print project names as table headers
+            foreach ($projectsManager->listOfStudents as $student) {
+                echo "<th>{$student->firstName} {$student->lastName}</th>";
             }
-	
-	}
-	echo "</tr>";
-	
+            ?>
+        </tr>
+
+        <?php
+        // Print student names and X sign for selected projects
+        foreach ($projectsManager->listOfProjects as $project) {
+            echo "<tr><td>{$project->name}</td>";
+            foreach ($projectsManager->listOfStudents as $student) {
+                if (isset($projectsManager->record[$project->name])) {
+                    if (in_array(array($student->firstName, $student->lastName), $projectsManager->record[$project->name])) {
+                        echo "<td>X</td>";
+                    } else {
+                        echo "<td></td>";
+                    }
+                } else {
+                    echo "<td></td>";
+                }
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+    <br>
+    <br>
+
+    <form action="index.php">
+        <input type="submit" value="Back to Home Page" name="back">
+    </form>
+<?php
+} else {
+    echo "Session variable 'projectManager' is not set.";
 }
-
-echo "</table>";
-
-
 ?>
